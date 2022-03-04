1464bfb5212ca10eb8398ebf57db8e32cf8b2615
SakuraKkkkk <526080338@qq.com>
2022-03-05 00:19:58 +0800

    添加主页面

--- src/pages/index/home/home.vue
+++ src/pages/index/home/home.vue
@@ -1,8 +1,9 @@
 <template>
 	<view>
-		<TopBar></TopBar>
+		<!-- <TopBar></TopBar> -->
 		<BottomNavigation></BottomNavigation>
-		<view style="margin-top: 85vh;margin-left: 25vw;">bug反馈：248823307(qq)</view>
+		<image src="https://images-cn.ssl-images-amazon.com/images/G/28/BOOK-Catalog/content/ACERW_B01LWXU304_0001_20160930.jpg" style="width: 100vw; height:400vw ;margin-top: 5vh;"></image>
+		<view style="margin-bottom:15vh;margin-left: 25vw;">bug反馈：248823307(qq)</view>
 	</view>
 </template>
 
